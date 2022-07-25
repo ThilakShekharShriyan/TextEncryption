3506715b1a25cdb0d71f2e4fad8839a97795ea9c
Thilak Shekhar Shriyan <thilakshekharshriyan@Thilaks-MacBook-Pro.local>
2022-07-25 19:33:02 +0530

    made skeletopn structre

---
+++ text-cipher/service/encryption.js
@@ -0,0 +1,33 @@
+
+
+
+const crypto =  require('crypto');
+
+// a random 256 bits hex value of len 32 characters
+const ENCRYPT_KEY = process.env.ENCRYPTION_KEY; 
+
+//iv length constant 16 for AES
+const iv_length = 16;
+
+
+//function to encrypt the text
+function encrypt(text){
+
+}
+
+
+//function to decrypt the text
+
+function decrypt(text){
+
+}
+
+
+
+
+
+
+
+
+
+
